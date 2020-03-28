15c04060d32ad5e7f09b94d2fcefd9b6ceb0b709
shanerbo <shanerbo@gmail.com>
2020-03-27 21:41:23 -0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDiK-KI8ngDM-LVkXJaUOjffGIJiDmSlKE',
+    authDomain: 'fake-money-tree.firebaseapp.com',
+    databaseURL: 'https://fake-money-tree.firebaseio.com',
+    projectId: 'fake-money-tree',
+    storageBucket: 'fake-money-tree.appspot.com',
+    messagingSenderId: '236698827163',
+    appId: '1:236698827163:web:f3fb3ac827b0e9801b493e'
+  }
+
 };
