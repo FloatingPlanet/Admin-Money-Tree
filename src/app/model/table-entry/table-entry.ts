5d6bd427a45426f5bc1e7541c4726ca1b379e49e
shanerbo <shanerbo@gmail.com>
2020-03-31 20:49:07 -0700

    add coupons service and remove table_entry

--- src/app/model/table-entry/table-entry.ts
+++
@@ -1,3 +0,0 @@
-export abstract class SmartTableEntry {
-  abstract getData(): any[];
-}
---
+++ src/app/services/coupons/coupons.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Coupon } from 'src/app/models/coupon';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CouponsService {
+  private Coupons: AngularFirestoreCollection<Coupon>;
+  public allCoupons: Coupon[];
+
+  constructor(private db: AngularFirestore) {
+    this.Coupons = this.db.collection('Coupons', ref => ref.orderBy('addedAt').limit(100));
+    this.Coupons.valueChanges().subscribe((coupons) => {
+      this.allCoupons = coupons;
+    });
+  }
+
+  /*
+  return coupons observable
+   */
+  get couponsObservable() {
+    return this.Coupons.valueChanges();
+  }
+
+  /*
+  validate coupon @c with coupons in firebase
+   */
+  public validateCoupon(c: string) {
+    return new Promise((resolve, reject) => {
+      this.Coupons.doc(c).ref.get().then((doc) => {
+        if (doc.exists) {
+          resolve(doc.data());
+        } else {
+          reject(`${c} does not exist`);
+        }
+      }).catch((error) => {
+        console.error(error);
+        reject(`fetch doc ${c} failed`);
+      });
+    });
+  }
+
+  /*
+  delete @coupon from firebase
+   */
+  public deleteCoupons(coupon: string) {
+    return new Promise((resolve, reject) => {
+      this.Coupons.doc(coupon).delete().then(() => {
+        resolve(`remove Coupon ${coupon} succeed`);
+
+      }).catch((error) => {
+        console.log(error);
+        reject(`fetch doc ${coupon} failed`);
+      });
+    });
+  }
+
+  /*
+  add new @coupon in firebase
+   */
+  public addCoupon(coupon: Coupon) {
+    return new Promise((resolve, reject) => {
+      this.Coupons.doc(coupon.coupon).ref.get().then((doc) => {
+        this.Coupons.doc(coupon.coupon)
+          .set(coupon)
+          .catch(error => {
+            console.error(error);
+            reject('Add coupon failed');
+          });
+        resolve(`doc ${coupon.coupon} added`);
+      }).catch((error) => {
+        console.error(error);
+        reject(`fetch doc ${coupon.coupon} failed`);
+      });
+    });
+
+  }
+
+  /*
+  retrieve a coupon using @couponName
+   */
+  public fetchCoupon(couponName: string) {
+    return new Promise((resolve, reject) => {
+      this.Coupons.doc(couponName).ref.get().then((doc) => {
+        if (doc.exists) {
+          resolve(doc.data());
+        } else {
+          reject(`${couponName} does not exist`);
+        }
+      }).catch((error) => {
+        console.log(error);
+        reject(`fetch coupon ${couponName} failed`);
+      });
+    });
+  }
+}
